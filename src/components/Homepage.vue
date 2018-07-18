6614da934fa825a0172a5807b584ded0db9a227f
Parker Kimball <pkimball114@gmail.com>
2018-07-17 23:07:35 -0700

    Updated for v1.0.0 of Kimball Bros landing page

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,64 +1,59 @@
 <template>
-	<div class="hello">
-		<h1>{{ title }}</h1>
-		<h2>{{ taglines[Math.floor(Math.random() * taglines.length)].message }}</h2>
-		<ul>
-			<li v-for="link in links">
-				<a v-bind:href="link.url"
-					target="_blank"
-				>
-					{{ link.text }}
-				</a>
-			</li>
-			<br>
-		</ul>
-	</div>
+  <div>
+    <h1>{{ title }}</h1>
+    <h3>{{ taglines[Math.floor(Math.random() * taglines.length)].message }}</h3>
+    <ul>
+      <li v-for="link in links" v-bind:key="link.text">
+        <a v-bind:href="link.url"
+          target="_blank"
+        >
+          {{ link.text }}
+        </a>
+      </li>
+      <br>
+    </ul>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'Welcome',
-		data () {
-			return {
-				title: 'Kimball Bros',
-				taglines: [
-					{ message: 'dummy slogan 1' },
-					{ message: 'dummy slogan 2' },
-					{ message: 'dummy slogan 3' }
-				],
-				links: [
-					{ 	
-						text: 'Facebook',
-						url: 'http://facebook.com' 
-					},
-					{ 	
-						text: 'Twitter',
-						url: 'http://twitter.com' 
-					},
-					{ 	
-						text: 'Instagram',
-						url: 'http://instagram.com' 
-					}
-				]
-			}
-		}
-	}
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+      title: 'Kimball Bros',
+      taglines: [
+        { message: 'We \'dem boys' }
+      ],
+      links: [
+        {
+          text: 'Email',
+          url: 'mailto:kimballbros@gmail.com'
+        },
+        {
+          text: 'Github',
+          url: 'https://github.com/KimballBros'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	h1, h2 {
-		font-weight: normal;
-	}
-	ul {
-		list-style-type: none;
-		padding: 0;
-	}
-	li {
-		display: inline-block;
-		margin: 0 10px;
-	}
-	a {
-		color: #123191;
-	}
+  h1, h3 {
+    font-weight: normal;
+  }
+  ul {
+    font-size: 14px;
+    list-style-type: none;
+    padding: 0px;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #123191;
+  }
 </style>
